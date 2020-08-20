f74a5bdf420b31d0959a5f508ebe320bf8056e3d
潘羿 <33997505+paniy@users.noreply.github.com>
2020-08-20 20:45:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ OK="${Green}[OK]${Font}"
 Error="${Red}[错误]${Font}"
 
 # 版本
-shell_version="1.1.3.4.1.1"
+shell_version="1.1.3.4.1.2"
 shell_mode="None"
 version_cmp="/tmp/version_cmp.tmp"
 v2ray_conf_dir="/usr/local/etc/v2ray"
@@ -334,9 +334,13 @@ v2ray_install() {
     rm -rf /root/v2ray
 }
 v2ray_update() {
+    mkdir -p /root/v2ray
+    cd /root/v2ray || exit
+    wget -N --no-check-certificate https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+    wget -N --no-check-certificate https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
     if [[ -d /usr/local/etc/v2ray ]]; then
-        bash <(curl -L -O -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-        bash <(curl -L -O -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
+        bash install-release.sh --force
+        bash install-dat-release.sh --force
     else
         systemctl disable v2ray.service --now
         mv -f /etc/v2ray/ /usr/local/etc/
@@ -344,9 +348,11 @@ v2ray_update() {
         rm -rf /etc/systemd/system/v2ray.service
         rm -rf /lib/systemd/system/v2ray.service
         rm -rf /etc/init.d/v2ray
-        bash <(curl -L -O -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-        bash <(curl -L -O -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
+        bash install-release.sh --force
+        bash install-dat-release.sh --force
     fi
+    # 清除临时文件
+    rm -rf /root/v2ray
 }
 nginx_exist_check() {
     if [[ -f "/etc/nginx/sbin/nginx" ]]; then
