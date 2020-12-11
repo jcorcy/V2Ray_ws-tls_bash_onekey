a17e99f13807eef6c0852961319d0ea49ee9ca88
潘羿 <33997505+paniy@users.noreply.github.com>
2020-12-12 00:39:12 +0800

    Update install.sh
    权限简直有毒。。

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ Error="${Red}[错误]${Font}"
 Warning="${Red}[警告]${Font}"
 
 # 版本
-shell_version="1.2.2.5"
+shell_version="1.2.2.6"
 shell_mode="None"
 version_cmp="/tmp/version_cmp.tmp"
 xray_conf_dir="/usr/local/etc/xray"
@@ -365,9 +365,11 @@ web_camouflage() {
 }
 xray_privilege_escalation() {
     if [[ -n "$(grep "User=nobody" ${xray_systemd_file})" ]]; then
-        echo -e "${OK} ${GreenBG} 检测到Xray权限不足，将提高Xray权限至root ${Font}"
+        #echo -e "${OK} ${GreenBG} 检测到Xray权限不足，将提高Xray权限至root ${Font}"
         systemctl stop xray
-        sed -i "s/User=nobody/User=root/" ${xray_systemd_file}
+        #sed -i "s/User=nobody/User=root/" ${xray_systemd_file}
+        chmod -fR a+rw /var/log/xray/
+        chown -fR nobody:nobody /var/log/xray/
         systemctl daemon-reload
         systemctl start xray
         sleep 1
@@ -402,8 +404,7 @@ xray_install() {
         #bash install-dat-release.sh --force
         judge "安装 Xray"
         sleep 1
-        #xray_privilege_escalation
-        chmod -fR a+rw /var/log/xray/
+        xray_privilege_escalation
     else
         echo -e "${Error} ${RedBG} Xray 安装文件下载失败，请检查下载地址是否可用 ${Font}"
         exit 4
@@ -426,7 +427,7 @@ xray_update() {
         sleep 1
         bash <(curl -L -s https://raw.githubusercontent.com/XTLS/Xray-install/main/install-release.sh)
         sleep 1
-        #xray_privilege_escalation
+        xray_privilege_escalation
     else
         echo -e "${GreenBG} 若更新无效，建议直接卸载再安装！ ${Font}"
         systemctl stop xray
@@ -440,7 +441,7 @@ xray_update() {
         sleep 1
         bash <(curl -L -s https://raw.githubusercontent.com/XTLS/Xray-install/main/install-release.sh)
         sleep 1
-        #xray_privilege_escalation
+        xray_privilege_escalation
     fi
     # 清除临时文件
     ##rm -rf /root/xray
